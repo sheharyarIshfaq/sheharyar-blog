7e06dbb165021fdb20a334e25067b8db72a68845
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-08-04 11:25:22 +0500

    Added Theme Toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
       <Script
         defer
         src="https://cloud.umami.is/script.js"
---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+import { ModeToggle } from "./ToggleMode";
+
+const Navbar = () => {
+  return (
+    <nav className="container flex justify-between items-center py-6 border-b border-slate-200">
+      <Link href="/" className="text-3xl font-bold">
+        Sheharyar's <span className="text-blue-500">Blog</span>
+      </Link>
+      <ModeToggle />
+    </nav>
+  );
+};
+
+export default Navbar;
